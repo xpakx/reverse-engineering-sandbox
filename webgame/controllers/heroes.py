2f918b122f6bf0ac6abb7c6f50e01f719ec78c7b
xpakx <55586470+xpakx@users.noreply.github.com>
2025-04-26 18:27:52 +0200

    feat: calculate hero stats based on level and stars

--- webgame/controllers/heroes.py
+++ webgame/controllers/heroes.py
@@ -1,4 +1,12 @@
+from typing import NamedTuple
+import json
+
 userHeroes = [3]
+attrs = ["strength", "physicalCritChance", "physicalAttack",
+         "magicResist", "magicPower", "magicPenetration",
+         "lifesteal", "intelligence", "hp",
+         "dodge", "armorPenetration", "armor",
+         "agility"]
 
 
 def getHeroById(id):
@@ -41,3 +49,110 @@ def getUserHeroes(request, temp):
     for id in userHeroes:
         result[str(id)] = getHeroById(id)
     return result
+
+
+class StatData(NamedTuple):
+    strength: int = 0
+    physicalCritChance: int = 0
+    physicalAttack: int = 0
+    magicResist: int = 0
+    magicPower: int = 0
+    magicPenetration: int = 0
+    lifesteal: int = 0
+    intelligence: int = 0
+    hp: int = 0
+    dodge: int = 0
+    armorPenetration: int = 0
+    armor: int = 0
+    agility: int = 0
+
+
+heroBaseData = {
+    3: StatData(intelligence=15, agility=20, strength=10, hp=500, physicalAttack=60),
+    }
+
+mainStat = {
+        3: "agility",
+    }
+
+heroStarData = {
+        3: [
+            StatData(intelligence=3, agility=4, strength=3),
+            StatData(intelligence=3, agility=7, strength=5),
+            StatData(intelligence=5, agility=10, strength=7),
+            StatData(intelligence=7, agility=15, strength=8),
+            StatData(intelligence=9, agility=20, strength=11),
+            StatData(intelligence=11, agility=24, strength=15),
+        ],
+    }
+
+
+class Hero:
+    def __init__(self, id: int):
+        base = heroBaseData[id]
+        self.strength = base.strength
+        self.physicalCritChance = base.physicalCritChance
+        self.physicalAttack = base.physicalAttack
+        self.magicResist = base.magicResist
+        self.magicPower = base.magicPower
+        self.magicPenetration = base.magicPenetration
+        self.lifesteal = base.lifesteal
+        self.intelligence = base.intelligence
+        self.hp = base.hp
+        self.dodge = base.dodge
+        self.armorPenetration = base.armorPenetration
+        self.armor = base.armor
+        self.agility = base.agility
+        self.mainStat = mainStat[id]
+        self.id = id
+
+    def processBaseStats(self):
+        self.physicalAttack = self.physicalAttack + 2 * self.agility + self.getMainAttr()
+        self.hp = self.hp + 40 * self.strength
+        self.armor = self.armor + self.agility
+        self.magicPower = self.magicPower + 3 * self.intelligence
+        self.magicResist = self.magicResist + self.intelligence
+
+    def getMainAttr(self):
+        if not hasattr(self, self.mainStat):
+            return 0
+        return getattr(self, self.mainStat)
+
+    def update(self, stat: StatData, times: int = 1):
+        for attr in attrs:
+            if not hasattr(self, attr) or not hasattr(stat, attr):
+                continue
+            new_value = getattr(self, attr) + getattr(stat, attr) * times
+            setattr(self, attr, new_value)
+
+    def applyStarsAndLevel(self, stars: int, level: int):
+        starsData = heroStarData[self.id][stars-1]
+        self.update(starsData, level)
+
+
+def getTestHero(id):
+    hero = getHeroById(id)
+    hero['agility'] = 27
+    hero['intelligence'] = 52
+    hero['hp'] = 700
+    hero['physicalAttack'] = 50
+    hero['strength'] = 32
+    hero['armor'] = 25
+    hero['magicPower'] = 25
+    hero['magicResist'] = 25
+    hero['skin'] = 0
+    hero['favorPetId'] = 0
+    hero['favorPower'] = 0
+    return hero
+
+
+def testHeroData(id: int, level: int, stars: int):
+    hero = Hero(id)
+    hero.applyStarsAndLevel(stars, level)
+    hero.processBaseStats()
+    resp = {}
+    for attr in attrs:
+        value = getattr(hero, attr)
+        resp[attr] = value
+    response = json.dumps(resp)
+    return response
