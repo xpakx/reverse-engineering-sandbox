a9e438c574b3d716edbb25c39968de850cc085db
xpakx <55586470+xpakx@users.noreply.github.com>
2025-05-30 22:48:49 +0200

    chore: remove unnecessary methods

--- webgame/controllers/items.py
+++ webgame/controllers/items.py
@@ -4,6 +4,7 @@ from repo.item import ItemDef
 
 userStamina = 0
 
+
 def buyStamina(request, repo: GameRepository, gameData):
     print(request)
     return {'stamina': 120}
@@ -28,19 +29,3 @@ def useStaminaItem(request, repo: GameRepository, gameData):
 
 def inventory(request, repo: GameRepository, gameData):
     return repo.getInventoryByUserId(1).toResponse()
-
-
-def addToInventory(repo: GameRepository, category: str, id: int, amount: int):
-    inventory = repo.getInventoryByUserId(1)
-    item = ItemDef(itemType=category, itemId=id, itemCount=amount)
-    inventory.addItem(item)
-
-
-def addMultToInventory(repo: GameRepository, reward):
-    for category in reward:
-        items = reward[category]
-        if isinstance(items, dict):
-            for item in items:
-                id = int(item)
-                amount = int(items[item])
-                addToInventory(repo, category, id, amount)
